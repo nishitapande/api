049d812da4a47ce755870b7f6c03088ada5238c3
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-05-06 22:35:13 +0530

    .

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -3,13 +3,7 @@ const cors = require("cors");
 const router = express.Router();
 const courseMiddleware = require("../middlewares/courseMiddleware");
 const authUtil = require("../util/authUtil");
-router.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT"],
-    credentials: true,
-  })
-);
+router.use(cors());
 
 router.get("/", courseMiddleware.getAllCourses);
 router.get("/:id", courseMiddleware.getCourseById);
