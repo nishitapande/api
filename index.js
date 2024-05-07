85237b3e7f28b36158db886d99b5cad428999125
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-05-07 17:18:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: true,
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   })
